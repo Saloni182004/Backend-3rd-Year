b58187821bcfb7aafd8617ff62a50cab5e977277
Saloni <18salonid2004@gmail.com>
2025-07-20 16:25:19 +0530

    first commit

--- ASSIGNMENT/assignment2.js
+++ ASSIGNMENT/assignment2.js
@@ -1,1 +1,211 @@
-// to do app :todo add krao using terminal add krana h file ke andr add krana h
+// // to do app :todo add krao using terminal add krana h file ke andr add krana h
+const readline = require('readline');
+const fs=require("fs");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const askQuestion = (question) => {
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+let existTodo=[];
+function readExistingTodo(){
+    return new Promise((resolve, reject)=>{
+        fs.readFile("./Todo.txt", "utf-8", function(err, data){
+            if(err) return reject(err);
+            existTodo=JSON.parse(data);
+             resolve();
+        })
+    })
+    
+}
+
+function addTodo(){
+    return new Promise(async (resolve, reject)=>{
+         const Title=await askQuestion("ENter Title: ");
+         const Description =await askQuestion("Enter Description: ");
+     const todo={
+        Title,
+        Description
+    }
+    existTodo.push(todo);
+    fs.writeFile("./Todo.txt", JSON.stringify(existTodo), function(err){
+        if(err) return reject(err);
+        console.log("Todo added");
+        rl.close();
+        resolve();
+    })
+    })
+}
+
+function deleteTodo(){
+    return new Promise(async (resolve, reject)=>{
+        const title= await askQuestion("Enter title: ");
+        existTodo=existTodo.filter((todo)=>{
+            return todo.Title !== title
+        })
+        fs.writeFile("./Todo.txt", JSON.stringify(existTodo), function(err){
+            if(err) return reject(err);
+            console.log("Todo Deleted");
+        })
+        rl.close();
+        resolve();
+    })
+}
+
+function showTodo(){
+    return new Promise((resolve, reject)=>{
+        fs.readFile("./Todo.txt", "utf-8", function(err, data){
+            if(err) return reject(err);
+            existTodo=JSON.parse(data);
+            console.log(existTodo);
+             resolve();
+        })
+    })  
+}
+
+async function inputTodo(){
+    try{
+    await readExistingTodo();
+    await addTodo();
+    }catch(error){
+        console.log(error);
+        rl.close();
+    }
+    
+}
+// inputTodo();
+
+async function deleteTODO() {
+    try{
+        await readExistingTodo();
+        await deleteTodo();
+    }catch(error){
+        console.log(error);
+        rl.close();
+    }
+}
+// deleteTODO();
+async function show(){
+    try{
+        await showTodo();
+    } catch(error){
+        console.log(error);
+    }
+    
+}
+show();
+
+
+// const readline = require('readline');
+// const fs = require("fs");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// const askQuestion = (question) => {
+//   return new Promise((resolve) => {
+//     rl.question(question, (answer) => {
+//       resolve(answer);
+//     });
+//   });
+// };
+
+// let existTodo = [];
+
+// function readExistingTodo() {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile("./Todo.txt", "utf-8", function (err, data) {
+//       if (err) {
+//         if (err.code === "ENOENT") {
+//           existTodo = [];
+//           return resolve();
+//         } else {
+//           return reject(err);
+//         }
+//       }
+//       existTodo = JSON.parse(data || "[]");
+//       resolve();
+//     });
+//   });
+// }
+
+// function addTodo() {
+//   return new Promise(async (resolve, reject) => {
+//     const Title = await askQuestion("Enter Title: ");
+//     const Description = await askQuestion("Enter Description: ");
+//     const todo = { Title, Description };
+//     existTodo.push(todo);
+//     fs.writeFile("./Todo.txt", JSON.stringify(existTodo, null, 2), function (err) {
+//       if (err) return reject(err);
+//       console.log("Todo added successfully!");
+//       resolve();
+//     });
+//   });
+// }
+
+// function deleteTodo() {
+//   return new Promise(async (resolve, reject) => {
+//     const title = await askQuestion("Enter title to delete: ");
+//     const initialLength = existTodo.length;
+//     existTodo = existTodo.filter((todo) => todo.Title !== title);
+//     if (existTodo.length === initialLength) {
+//       console.log("No todo found with that title.");
+//     } else {
+//       fs.writeFile("./Todo.txt", JSON.stringify(existTodo, null, 2), function (err) {
+//         if (err) return reject(err);
+//         console.log("Todo deleted successfully!");
+//       });
+//     }
+//     resolve();
+//   });
+// }
+
+// function showTodo() {
+//   return new Promise((resolve, reject) => {
+//     if (existTodo.length === 0) {
+//       console.log("No todos found.");
+//     } else {
+//       console.log("\nYour Todos:");
+//       existTodo.forEach((todo, index) => {
+//         console.log(`\n${index + 1}.  ${todo.Title}\n   ${todo.Description}`);
+//       });
+//     }
+//     resolve();
+//   });
+// }
+
+// async function main() {
+//   try {
+//     await readExistingTodo();
+
+//     const choice = await askQuestion(
+//       "\nWhat would you like to do?\n1️ Add Todo\n2️ Delete Todo\n3️ Show Todos\n Enter choice (1/2/3): "
+//     );
+
+//     if (choice === "1") {
+//       await addTodo();
+//     } else if (choice === "2") {
+//       await deleteTodo();
+//     } else if (choice === "3") {
+//       await showTodo();
+//     } else {
+//       console.log("Invalid choice.");
+//     }
+//   } catch (err) {
+//     console.error("Error:", err);
+//   } finally {
+//     rl.close();
+//   }
+// }
+
+// main();
