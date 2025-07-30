2185116a12c6e72611e9d34ac94ece1746325de6
Saloni <18salonid2004@gmail.com>
2025-07-30 10:52:56 +0530

    first commit

--- LECTURE-11/dom.js
+++ LECTURE-11/dom.js
@@ -3,7 +3,7 @@
 //2. using class
 //3. using tag
 //4. using query selector - we can pass pass class by using . , id by # and tag by using ful tag name
-
+/*
 let ele1=document.getElementById("heading");
 console.log(ele1);
 let ele2=document.getElementsByClassName("Item");
@@ -16,7 +16,7 @@ console.log(ele4);
 console.log(ele5);
 let ele6=document.querySelectorAll(".Item");
 console.log(ele6);
-let ul=document.querySelector("#container");
+let ul=document.querySelector("#container");*/
 
 //Properties
 /*
@@ -24,6 +24,7 @@ innerHtml
 innerText
 textContent
 */
+/*
 let data=ele4.innerText;
 console.log(data);
 ele4.innerText="Changed using js";
@@ -38,17 +39,28 @@ console.log(data4);
 
 ul.innerHTML=`<li class="Item">Item4</li>
               <li class="Item">Item5</li>
-              <li class="Item">Item6</li>`
+              <li class="Item">Item6</li>`*/
 
 /*
 getAttribute
 setAttribute
 classList
 */
+/*
 let ele5=document.querySelector(".Item");
 console.dir(ele5);
 console.dir(ele5.getAttribute("id"));
 console.dir(ele5.getAttribute("class"));
 ele5.setAttribute("id","js");
 console.log(ele5.getAttribute("id"));
-console.log(ele5.classList.contains("Item"))
+ele5.classList.add("delete")
+console.log(ele5.classList.contains("delete"));
+ele5.classList.remove("Item");
+console.log(ele5.classList);
+*/
+
+let sign=document.querySelector('.Signup');
+let form=document.querySelector('#form');
+sign.addEventListener('click',function(){
+    form.classList.toggle('hide');
+})
