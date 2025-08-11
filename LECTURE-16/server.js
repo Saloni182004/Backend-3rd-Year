5a100a0c07bb600c711eb3a5389be9ac6e56101e
Saloni <18salonid2004@gmail.com>
2025-08-11 19:12:14 +0530

    first commit

--- LECTURE-16/server.js
+++ LECTURE-16/server.js
@@ -1,9 +1,13 @@
 const express=require("express");
 const mongoose=require("mongoose");
+
 const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
 const Blogs=require("./model/user")
+const Users=require("./model/userSchema")
+
 app.post("/blogs",async(req,res)=>{
     let {title,body}=req.body;
     let newBlog=new Blogs({
@@ -35,6 +39,39 @@ app.get("/blogs/:id",async(req,res)=>{
         data:blog
     })
 })
+
+app.post("/users", async (req, res) => {
+  let { email, username, password } = req.body;
+  let newUser = new Users({
+    email: email,
+    username: username,
+    password: password
+  });
+  await newUser.save();
+  res.json({
+    success: true,
+    data: newUser,
+    message: "user added successfully!!!"
+  });
+});
+
+app.get("/users", async (req, res) => {
+  let allUsers = await Users.find();
+  res.json({
+    success: true,
+    data: allUsers
+  });
+});
+
+app.get("/users/:id", async (req, res) => {
+  let { id } = req.params;
+  let user = await Users.findOne({ _id: id });
+  res.json({
+    success: true,
+    data: user
+  });
+});
+
 app.listen(3000,()=>{
     console.log("Server started");
 })
