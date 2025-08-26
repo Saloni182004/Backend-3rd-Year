fe8677723b2d776f827e99e1dd93bbb580f753b2
Saloni <18salonid2004@gmail.com>
2025-08-26 15:50:43 +0530

    first commit

---
+++ LECTURE-23/middleware/token.js
@@ -0,0 +1,29 @@
+const jwt=require("jsonwebtoken");
+function isLogin(req,res,next){
+    if(!req.headers.authorization)(
+        res.json({
+            success:false,
+            message:"no authoization"
+        })
+    )
+    let token = req.headers.authorization;
+    if (!token) {
+        return res.json({
+            success: false,
+            message: "Please login"
+        });
+    }
+    
+    const decode=jwt.verify(token, "blogs");
+    if(!decode){
+    return res.json({
+        success:false,
+        message:"Invalid token"
+    })
+    }
+    req.userId = decode.userId;
+    next();
+        
+}
+    
+module.exports = { isLogin };
