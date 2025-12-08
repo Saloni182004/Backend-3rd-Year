73e617b9892fb8c6c332272cb9b64597a7244a64
Saloni <18salonid2004@gmail.com>
2025-12-08 14:12:58 +0530

    first commit

---
+++ LECTURE-41/server.test.js
@@ -0,0 +1,44 @@
+const User=require("./model/user.model");
+const mongoose=require("mongoose");
+let app=require("./server");
+let request=require("supertest");
+let {MongoMemoryServer}=require("mongodb-memory-server");
+let mongoServer;
+beforeAll(async()=>{
+    mongoServer=await MongoMemoryServer.create();
+    let url=mongoServer.getUri();
+    await mongoose.connect(url);
+})
+afterEach(async()=>{
+    await User.deleteMany();
+})
+afterAll(async()=>{
+    await mongoose.disconnect();
+    await mongoServer.stop();
+})
+describe("POST /api/users/register",()=>{
+    it("should return user already exist if email is s@gmail.com",async()=>{
+        await User.create({
+            name:"Saloni",
+            email:"s@gmail.com",
+            password:"1234"
+        })
+        let response=await request(app).post("/api/users/register").send({
+            name:"Saloni",
+            email:"s@gmail.com",
+            password:"1234"
+        })
+        expect( response.body.message).toBe("User already exists")
+    })
+    it("should create a new user with s@gmail.com",async()=>{
+        let response=await request(app).post("/api/users/register").send({
+            name:"Saloni",
+            email:"s@gmail.com",
+            password:"1234"
+        })
+        let userData=await User.findById(response.body.data._id);
+        expect(response.body.data._id).toBe(userData._id.toString());
+        expect(response.body.data.name).toBe(userData.name);
+        expect(response.body.data.email).toBe(userData.email);
+    })
+})
