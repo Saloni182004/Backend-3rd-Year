7259ab908904b8c47593f5fdcced4a27777c1b7a
Saloni <18salonid2004@gmail.com>
2025-12-09 14:12:11 +0530

    first commit

---
+++ LECTURE-42/server.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+app.listen(3000,()=>{
+    console.log("server started");
+})
